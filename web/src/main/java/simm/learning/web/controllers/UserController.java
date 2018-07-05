dd183acf1f1f407de541922f2905cadffb57f4a9
git-simm <214382122@qq.com>
2018-07-05 20:43:13 +0800

    Revert "2"
    This reverts commit 59855d2135e611eaae7af2829cd7967240a633c6.

--- web/src/main/java/simm/learning/web/controllers/UserController.java
+++ web/src/main/java/simm/learning/web/controllers/UserController.java
@@ -27,10 +27,5 @@ public class UserController {
     public String say(@PathVariable String name) {
         return "你好，" + name;
     }
-
-    @RequestMapping("/say2/{name}")
-    public String say2(@PathVariable String name) {
-        return "hello ，" + name;
-    }
 }
 
