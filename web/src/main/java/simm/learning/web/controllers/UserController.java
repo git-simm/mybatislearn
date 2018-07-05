c828f4e4abc93b227d99876f7a5034843a4086a6
git-simm <214382122@qq.com>
2018-07-05 21:04:16 +0800

    Revert "Revert "Revert "dev添加一个新方法"""
    This reverts commit 7a3367e6476dfe52f48ae2f0278dee0928a7137f.

--- web/src/main/java/simm/learning/web/controllers/UserController.java
+++ web/src/main/java/simm/learning/web/controllers/UserController.java
@@ -2,7 +2,6 @@ package simm.learning.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import simm.learning.biz.entity.User;
@@ -15,17 +14,11 @@ import java.util.List;
 public class UserController {
     @Autowired
     UserMapper userMapper;
-
     @RequestMapping("/list")
     @ResponseBody
-    public List<User> getList() {
+    public List<User> getList(){
         List<User> users = userMapper.selectAll();
         return users;
-    }
-
-    @RequestMapping("/say/{name}")
-    public String say(@PathVariable String name) {
-        return "你好，" + name;
     }
 }
 
