ff81bfb151a9d35fa57a2957f7ffc846899803c9
git-simm <214382122@qq.com>
2018-07-05 20:45:35 +0800

    Revert "dev添加一个新方法"
    This reverts commit 6a0d8fba772181882e10d98c5648ea09ea3859b1.

--- biz/src/main/java/simm/learning/biz/mapper/UserMapper.java
+++ biz/src/main/java/simm/learning/biz/mapper/UserMapper.java
@@ -1,10 +1,8 @@
 package simm.learning.biz.mapper;
-import org.springframework.stereotype.Service;
 import simm.learning.biz.entity.User;
 
 import java.util.List;
 
-@Service
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
 
